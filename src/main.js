9034c92634774c2ec6b8415df142318e4285c4ee
SEONKYU\difke <somedaycode@gmail.com>
2021-03-24 17:45:08 +0900

    Add. productView 클래스 main.js에 추가

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import { _ } from './js/util.js';
 import { ProductView } from './js/views/ProductView.js';
 import { productModel } from './js/models/ProductModel.js';
+import { walletModel } from './js/models/WalletModel.js';
 
 const DOMTargets = {
   productLists: _.$('.products'),
 };
 
 const main = () => {
-  new ProductView(DOMTargets, productModel);
+  new ProductView(DOMTargets, productModel, walletModel);
 };
 
 main();
