3e646d78c989da1e4cf1b228f891724b01ad2c0d
SEONKYU\difke <somedaycode@gmail.com>
2021-03-24 16:46:25 +0900

    Add. ProductView, renderDefaultProductTemplate 함수 추가
    자바스크립트에서 동적으로 HTML을 렌더링 해준다. 그에 따라 style도 수정하였음

--- src/js/views/ProductView.js
+++ src/js/views/ProductView.js
@@ -1,7 +1,25 @@
+import { _, insertTemplate } from '../util';
+import { productsTemplate } from '../templates/HTMLTemplates.js';
+
 class ProductView {
-  constructor(model) {
-    this.model = model;
+  constructor({ productLists }, productModel) {
+    this.productModel = productModel;
+    this.products = productModel.products;
+    this.$productLists = productLists;
+    this.init();
   }
 
-  init() {}
+  init() {
+    this.renderDefaultProductLists();
+  }
+
+  renderDefaultProductLists() {
+    const defaultTemplate = this.products.reduce((prev, product) => {
+      const template = productsTemplate(product.name, product.price);
+      return prev + template;
+    }, '');
+    insertTemplate(this.$productLists, 'beforeend', defaultTemplate);
+  }
 }
+
+export { ProductView };
