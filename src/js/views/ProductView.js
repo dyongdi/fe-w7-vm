e598fa4906ae7f1b182631fbc1e8d8e779bff512
SEONKYU\difke <somedaycode@gmail.com>
2021-03-27 00:46:51 +0900

    Add. productView.js 재고관리 메소드 추가

--- src/js/views/ProductView.js
+++ src/js/views/ProductView.js
@@ -23,9 +23,11 @@ class ProductView {
   handleClickProduct({ target }) {
     if (!target.closest('.available')) return;
     const product = target.closest('.product__name');
+    const productName = product.textContent.trim();
     const productPrice = product.nextElementSibling.textContent;
     this.walletModel.useCurrentInsertMoney(productPrice);
     this.walletModel.debounceReturnMoney();
+    this.productModel.buyProduct(productName);
   }
 
   renderDefaultProductLists() {
@@ -38,13 +40,20 @@ class ProductView {
 
   updateView({ currentInsertMoney }) {
     const priceLists = this.getPriceLists();
+    const allProducts = this.getProducts();
     this.clearProductAvailability(priceLists, currentInsertMoney);
     this.isProductAvailable(priceLists, currentInsertMoney);
+    this.checkInventory(allProducts);
   }
 
   getPriceLists() {
     const priceLists = Array.from(_.$All('.price'));
     return priceLists;
+  }
+
+  getProducts() {
+    const allProducts = Array.from(_.$All('.product__name'));
+    return allProducts;
   }
 
   isProductAvailable(priceLists, currentInsertMoney) {
@@ -62,11 +71,38 @@ class ProductView {
     products.forEach((product) => _.rmClass(product, 'available'));
   }
 
+  isOutofStock(products) {
+    products.forEach((product) => {
+      _.addClass(product, 'zero-inventory');
+      product.disabled = true;
+    });
+  }
+
   clearProductAvailability(priceLists) {
     const productSelected = priceLists.map(
       (price) => price.previousElementSibling
     );
     this.removeAvailability(productSelected);
+  }
+
+  getZeroProductsNames() {
+    const zeroProductsName = this.productModel.products
+      .filter((product) => product.inventory === 0)
+      .map((product) => product.name);
+    return zeroProductsName;
+  }
+
+  checkInventory(allProducts) {
+    const zeroProductsNames = this.getZeroProductsNames();
+    const productsOutofStock = zeroProductsNames
+      .map((productName) => {
+        return allProducts.filter(
+          (product) => product.textContent.trim() === productName
+        );
+      })
+      .flat();
+    if (productsOutofStock.length === 0) return;
+    this.isOutofStock(productsOutofStock);
   }
 }
 
